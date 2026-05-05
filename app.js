13de1f03acc24988a69c89bdbf9e4f1dfb131774
jamaldeenusman4life <usmanjamal2009@gmail.com>
2026-05-05 23:49:20 +0100

    feat: initial project setup and server configuration

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+
+const app = express();
+
+app.use(cors());
+app.use(morgan("dev"));
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({ message: "Supermarket API is running" });
+});
+
+module.exports = app;
