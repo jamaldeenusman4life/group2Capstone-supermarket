dd4541e21dab238d4ad6cb0024b7f4c6c9ac8769
jamaldeenusman4life <usmanjamal2009@gmail.com>
2026-05-20 15:05:43 +0100

    feat: add email notifications for registration, order confirmation, order status and low stock alert

--- src/utils/sendEmail.js
+++ src/utils/sendEmail.js
@@ -3,12 +3,18 @@ import nodemailer from "nodemailer";
 const sendEmail = async ({ to, subject, html }) => {
   try {
     const transporter = nodemailer.createTransport({
-      service: "gmail",
+      host: "smtp.gmail.com",
+      port: 465,
+      secure: true,
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASSWORD,
       },
     });
+
+    await transporter.verify();
+
+    console.log("Email transporter is ready to send messages");
 
     const mailOptions = {
       from: `"Group 2 Supermarket" <${process.env.EMAIL_USER}>`,
